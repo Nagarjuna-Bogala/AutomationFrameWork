8262d6dd320781fb7fe12eaafb2aeefba531e2d7
Nagarjuna-Bogala <nagarjunabogala7981@gmail.com>
2022-09-12 12:47:51 +0530

    commit and push by user 1

---
+++ src/test/java/sampleTests/DemoTest.java
@@ -0,0 +1,13 @@
+package sampleTests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("This is Demo");
+	}
+
+}
---
+++ src/test/java/sampleTests/SampleTest.java
@@ -0,0 +1,13 @@
+package sampleTests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void smpleTest()
+	{
+		System.out.println("This is sample");
+	}
+
+}
